fa408448f512767b0d6771d3498388e57646b016
innogator <btanne6@gmail.com>
2020-01-18 19:06:57 -0500

    Add python stuff

--- configure-linux.sh
+++ configure-linux.sh
@@ -22,12 +22,22 @@ git config --global user.email "${GITEMAIL}"
 echo 'Git has been configured!'
 git config --list
 
+git config --global credential.helper store
+
+
 # neovim
 sudo apt install neovim -y
+git config --global core.editor nvim
+
+# tmux
 sudo apt install tmux -y
 
 # zsh
 sudo apt install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# python
+sudo apt install python-pip -y
+sudo apt install python3-pip -y
 
